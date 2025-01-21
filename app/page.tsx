c711b1f4a6db2d1716f1dda8f87953fcc9f405d0
Srinu126 <srinurachakonda126@gmail.com>
2025-01-21 14:06:54 -0500

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Service Nexus</h1>;
+  return (
+    <div>
+      <h1>Service Nexus</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
