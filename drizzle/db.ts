6652c33562a2ac3194e62f44fbf7e2531608adc0
Srinu126 <srinurachakonda126@gmail.com>
2025-01-29 09:31:22 -0500

    modified env

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,18 +1,16 @@
-
 import dotenv from "dotenv";
-import {drizzle} from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@/drizzle/schema";
 import { Pool } from "pg";
 
 dotenv.config();
 
-
 export const client = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "srinu",
-    database: "servicenexusone",
+  host: process.env.HOST,
+  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-export const db = drizzle(client, {schema});
+export const db = drizzle(client, { schema });
